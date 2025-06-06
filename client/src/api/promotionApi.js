5f2778f0bd4eefcba55aeeaf92ae9aa2ef300e15
thuyoanhhh1312 <thuyoanhhh1312@gmail.com>
2025-06-07 03:31:53 +0700

    fix

--- client/src/api/promotionApi.js
+++ client/src/api/promotionApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import axiosInstance from './axiosInstance';
 
-const API_URL = process.env.API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const getPromotions = async () => {
     try {
